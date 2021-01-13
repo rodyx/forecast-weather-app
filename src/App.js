5b65e44e2e749ad36ba8e3eebabcf1bd02af00db
rodyx <erik.valeev.ev@gmail.com>
2021-01-13 10:56:34 +0500

    safari improves

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Main } from './pages/Main';
-import { CSSTransition} from 'react-transition-group';
+import { CSSTransition } from 'react-transition-group';
 
 function App() {
   return (
     <div className="App">
-       <CSSTransition
-            appear
-            in
-            classNames='backgroundTransition'
-            timeout={1000}>
-              
-              <Main />
-      
+      <CSSTransition
+        appear
+        in
+        classNames='backgroundTransition'
+        timeout={1000}>
+
+        <Main />
+
       </CSSTransition>
     </div>
   );
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,69 +1,65 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './styles.module.scss';
-import {WeatherData} from '../components/WeatherData/WeatherData'
+import { WeatherData } from '../components/WeatherData/WeatherData'
 import { Landscape } from '../components/Landscape/Landscape';
-import {FetchWeatherData} from '../api/openweather';
+import { FetchWeatherData } from '../api/openweather';
 import { createClient } from 'pexels';
-
 
 export const Main = () => {
     const [city, setCity] = useState('Ташкент');
     const [imgUrl, setBackground] = useState('https://wallpaperaccess.com/full/1093687.png')
     const [weather, setWeather] = useState(null);
-    const [photo,setPhoto] = useState(null);
-    
+    const [photo, setPhoto] = useState(null);
+
     const handleFormSubmit = (e) => {
-        e.preventDefault()
-        console.log(e.currentTarget.city)
-        setCity(e.currentTarget.city.value)
-    }
+        e.preventDefault();
+        console.log(e.currentTarget.city);
+        setCity(e.currentTarget.city.value);
+    };
 
     useEffect(() => {
         if (city) {
-            const client = createClient('563492ad6f91700001000001e1efacc9129f40259a028a356019f245');
-            
-            client.photos.search({ query: city, orientation: 'landscape','locale': 'ru-RU' })
-                .then(arr => {
+            const client = createClient(
+                "563492ad6f91700001000001e1efacc9129f40259a028a356019f245"
+            );
+
+            client.photos
+                .search({ query: city, orientation: "landscape", locale: "ru-RU" })
+                .then((arr) => {
                     let index = null;
-                    
+
                     if (arr.photos?.length) {
-                        
                         index = Math.floor(Math.random() * arr.photos.length);
-                        
-                        setPhoto(arr.photos[index])
+
+                        setPhoto(arr.photos[index]);
                         setBackground(arr.photos[index].src.large2x);
                     }
-                
                 });
-                
-            FetchWeatherData(city)
-                .then(setWeather)
-            
-                
+
+            FetchWeatherData(city).then(setWeather);
         }
-    }, [city])
+    }, [city]);
 
     const background = {
-        backgroundImage : `url(${imgUrl})`,
+        backgroundImage: `url(${imgUrl})`,
     }
 
-return (
+    return (
         <div className={styles.mainContainer} style={background}>
-                { weather && <Landscape weather={weather} photo={photo}/> }
+            { weather && <Landscape weather={weather} photo={photo} />}
             <div className={styles.information}>
                 <form onSubmit={handleFormSubmit} className={styles.inputSearch}>
-                        <input 
-                            placeholder="Другой город.." 
-                            type="text" 
-                            name="city"
-                        />
+                    <input
+                        placeholder="Другой город.."
+                        type="text"
+                        name="city"
+                    />
                     <button type="submit">
                         <i className="fas fa-search"></i>
                     </button>
                 </form>
-                { weather && <WeatherData weather={weather}/>}
-                
-             </div>
+                {weather && <WeatherData weather={weather} />}
+            </div>
         </div>
-   )
+    )
 }
